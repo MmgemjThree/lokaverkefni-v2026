bf8040532defe55835029d68708f5018a3562a97
MmgemjThree <mmagnus190@gmail.com>
2026-05-06 10:35:23 +0000

    CSS now works, still database issues plus adjusted some colors

--- db/insert.sql
+++ db/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO shows (name, description, firstAired, episodes, length) VALUES
 ('Mock the week', 'British comedy panel show', '05/06/2005', 245, '29-45 minutes')
+
+INSERT INTO shows (name, description, firstAired, episodes, length) VALUES
+('The Big Bang Theory', 'American sitcom', '24/9/2007', '279', '18-27 minutes')
