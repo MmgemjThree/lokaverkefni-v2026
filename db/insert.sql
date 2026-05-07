e12afbda392cab0101ccc08a2ccf40d6c4f33e5b
MmgemjThree <mmagnus190@gmail.com>
2026-05-07 10:40:36 +0000

    HTML should work, images will be properly added soon

--- db/insert.sql
+++ db/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO shows (name, description, firstAired, episodes, length) VALUES
-('Mock the week', 'British comedy panel show', '05/06/2005', 245, '29-45 minutes')
+INSERT INTO shows (name, description, firstAired, episodes, length, image_url) VALUES
+('Mock the week', 'British comedy panel show', '05/06/2005', 245, '29-45 minutes', 'MTW.jpg'),
+('The Big Bang Theory', 'American sitcom', '24/9/2007', '279', '18-27 minutes', 'BBT.jpg')
 
-INSERT INTO shows (name, description, firstAired, episodes, length) VALUES
-('The Big Bang Theory', 'American sitcom', '24/9/2007', '279', '18-27 minutes')
+SELECT * FROM shows
--- db/tables.sql
+++ db/tables.sql
@@ -4,25 +4,7 @@ CREATE TABLE IF NOT EXISTS shows (
     description TEXT,
     firstAired VARCHAR(100) NOT NULL,
     episodes INT NOT NULL,
-    length VARCHAR(30)
+    length VARCHAR(30),
+    image_url VARCHAR(500)
 );
-CREATE TABLE IF NOT EXISTS stars (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    birthDate VARCHAR(100),
-    description TEXT
-);
-CREATE TABLE IF NOT EXISTS starShow (
-    showID INT,
-    starID INT,
-    PRIMARY KEY (showID, starID),
-    CONSTRAINT fk_showsID
-        FOREIGN KEY (showID)
-        REFERENCES shows(id),
-    CONSTRAINT fk_starID
-        FOREIGN KEY (starID)
-        REFERENCES stars(id)
-);
-DROP TABLE TV_Shows
-DROP TABLE starShow
-DROP TABLE Stars
+DROP TABLE shows
